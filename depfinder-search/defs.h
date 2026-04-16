4e291a3046b697a4fe8658ee6f1c252ced609953
AbdurRahman Rozell <aryr100@gmail.com>
2026-04-16 10:40:17 -0400

    Update defs.h

--- depfinder-search/defs.h
+++ depfinder-search/defs.h
@@ -1,9 +1,14 @@
 // See COPYING for license information.
 
-// Defines to be used for loading file names.
+// Defines for temporary file paths and the package database directory.
 #ifndef TMPDIR
-#define TMPDIR		"/tmp/depfinder."
+#define TMPDIR    "/tmp/depfinder."
 #endif
 #ifndef TMPFILE
-#define TMPFILE(file,shellpid)	std::string(TMPDIR + std::string(shellpid) + std::string(file) )
+#define TMPFILE(file, shellpid) \
+    std::string(TMPDIR + std::string(shellpid) + std::string(file))
+#endif
+// Package database directory (Slackware-current uses /var/lib/pkgtools/packages/)
+#ifndef PKGDIR
+#define PKGDIR    "/var/lib/pkgtools/packages/"
 #endif
